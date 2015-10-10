3b0b519195b9d1d0bcecc66d1199f6a62d1f9662
Iago Effting <iago.effting@gmail.com>
2015-10-10 19:26:29 -0300

    Refactoring GenerationTest

--- tests/GenerationTest.php
+++ tests/GenerationTest.php
@@ -4,64 +4,57 @@ class GenerationTest extends TestCase
 {
 
   protected $twoFactor;
+  protected $data = [];
 
-  function testGenerateKeyIsNotEmpty()
+  public function setUp()
   {
+    parent::setUp();
+
+    $faker = Faker\Factory::create();
     $this->twoFactor = new \IagoEffting\TwoFactorAPI\TwoFactor();
     app()->instance('TwoFactor', $this->twoFactor);
 
+    // Data for test
+    $email = $faker->email;
+    $this->data = [
+      'mail' => $email,
+      'key' => strtoupper(str_random(32))
+    ];
+
+  }
+
+  function testGenerateKeyIsNotEmpty()
+  {
     $this->assertNotEmpty(TwoFactor::generateKey());
   }
 
   public function testKeyHaveNumberOfStrengthSecret()
   {
-    $this->twoFactor = new \IagoEffting\TwoFactorAPI\TwoFactor();
-    app()->instance('TwoFactor', $this->twoFactor);
-
     $numberOfChars = strlen(TwoFactor::generateKey());
-
-    $this->assertEquals(32, $numberOfChars);
+    $lengthOfSecret = 32;
+    $this->assertEquals($lengthOfSecret, $numberOfChars);
   }
 
   function testGenerateQrCodeIsNotNull()
   {
-    $this->twoFactor = new \IagoEffting\TwoFactorAPI\TwoFactor();
-    app()->instance('TwoFactor', $this->twoFactor);
-    $faker = Faker\Factory::create();
-
-    $key = strtoupper(str_random(32));
-
-    $data = [
-      'mail' => $faker->email,
-      'key' => $key
-    ];
-
-    $this->assertNotEmpty(TwoFactor::generateQrCode($data));
+    $this->assertNotEmpty(TwoFactor::generateQrCode($this->data));
   }
 
   function testGenerateUrlQrCodeIsNotEmpty()
   {
-    $this->twoFactor = new \IagoEffting\TwoFactorAPI\TwoFactor();
-    app()->instance('TwoFactor', $this->twoFactor);
-    $faker = Faker\Factory::create();
-
-    $key = strtoupper(str_random(32));
-
-    $data = [
-      'mail' => $faker->email,
-      'key' => $key
-    ];
-
     $company = config('twofactor.company_name');
-    $email   = urlencode($data['mail']);
-    $secret  = $data['key'];
+    $mail = urlencode($this->data['mail']);
+    $urlQrCode = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2F{$company}%3A{$mail}%3Fsecret%3D{$this->data['key']}%26issuer%3D$company";
 
-    $urlQrCode = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2F{$company}%3A{$email}%3Fsecret%3D{$secret}%26issuer%3D$company";
+    $this->assertContains(TwoFactor::generateQrCode($this->data), $urlQrCode);
+  }
 
-    $this->assertContains(TwoFactor::generateQrCode($data), $urlQrCode);
+  public function tearDown()
+  {
+    parent::tearDown();
 
-   // TODO: REGEX
-   // $this->assertRegExp('', TwoFactor::generateQrCode($data));
+    unset($this->data);
+    unset($this->twoFactor);
   }
 
 
