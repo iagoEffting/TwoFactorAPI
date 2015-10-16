1829b03957adff6018b6a2475d5901ca00cb4a08
Iago Effting <iago.effting@gmail.com>
2015-10-16 19:35:33 -0300

    Refactoring tests

--- tests/GenerationTest.php
+++ tests/GenerationTest.php
@@ -1,61 +1,102 @@
 <?php
 
+/**
+ * Class GenerationTest
+ */
 class GenerationTest extends TestCase
 {
 
-  protected $twoFactor;
-  protected $data = [];
+    protected $twoFactor;
+    protected $data = [];
 
-  public function setUp()
-  {
-    parent::setUp();
 
-    $faker = Faker\Factory::create();
-    $this->twoFactor = new \IagoEffting\TwoFactorAPI\TwoFactor();
-    app()->instance('TwoFactor', $this->twoFactor);
+    /**
+     * Configuration test
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
 
-    // Data for test
-    $email = $faker->email;
-    $this->data = [
-      'mail' => $email,
-      'key' => strtoupper(str_random(32))
-    ];
+        $faker = Faker\Factory::create();
 
-  }
+        $this->twoFactor = new \IagoEffting\TwoFactorAPI\TwoFactor();
+        app()->instance('TwoFactor', $this->twoFactor);
 
-  function testGenerateKeyIsNotEmpty()
-  {
-    $this->assertNotEmpty(TwoFactor::generateKey());
-  }
+        $email = $faker->email;
 
-  public function testKeyHaveNumberOfStrengthSecret()
-  {
-    $numberOfChars = strlen(TwoFactor::generateKey());
-    $lengthOfSecret = 32;
-    $this->assertEquals($lengthOfSecret, $numberOfChars);
-  }
+        $this->data = [
+                       'mail' => $email,
+                       'key'  => strtoupper(str_random(32)),
+                      ];
 
-  function testGenerateQrCodeIsNotNull()
-  {
-    $this->assertNotEmpty(TwoFactor::generateQrCode($this->data));
-  }
+    }
 
-  function testGenerateUrlQrCodeIsNotEmpty()
-  {
-    $company = config('twofactor.company_name');
-    $mail = urlencode($this->data['mail']);
-    $urlQrCode = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2F{$company}%3A{$mail}%3Fsecret%3D{$this->data['key']}%26issuer%3D$company";
 
-    $this->assertContains(TwoFactor::generateQrCode($this->data), $urlQrCode);
-  }
+    /**
+     * Testing Key
+     *
+     * @return void
+     */
+    function testGenerateKeyIsNotEmpty()
+    {
+        $this->assertNotEmpty(TwoFactor::generateKey());
 
-  public function tearDown()
-  {
-    parent::tearDown();
+    }
 
-    unset($this->data);
-    unset($this->twoFactor);
-  }
+
+    /**
+     * Test secret strength
+     *
+     * @return void
+     */
+    public function testKeyHaveNumberOfStrengthSecret()
+    {
+        $numberOfChars  = strlen(TwoFactor::generateKey());
+        $lengthOfSecret = 32;
+        $this->assertEquals($lengthOfSecret, $numberOfChars);
+
+    }
+
+
+    /**
+     * Test QR URL
+     *
+     * @return void
+     */
+    function testGenerateQrCodeIsNotNull()
+    {
+        $this->assertNotEmpty(TwoFactor::generateQrCode($this->data));
+
+    }
+
+
+    /**
+     * Test if qr is empty
+     */
+    function testGenerateUrlQrCodeIsNotEmpty()
+    {
+        $company   = config('twofactor.company_name');
+        $mail      = urlencode($this->data['mail']);
+        $urlQrCode = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth%3A%2F%2Ftotp%2F{$company}%3A{$mail}%3Fsecret%3D{$this->data['key']}%26issuer%3D$company";
+
+        $this->assertContains(TwoFactor::generateQrCode($this->data), $urlQrCode);
+    }
+
+    /**
+     * Destructor Data
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        unset($this->data);
+        unset($this->twoFactor);
+
+    }
 
 
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,25 +1,32 @@
 <?php
 
+/**
+ * Class TestCase
+ */
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-  /**
-   * The base URL to use while testing the application.
-   *
-   * @var string
-   */
-  protected $baseUrl = 'http://localhost';
 
-  /**
-   * Creates the application.
-   *
-   * @return \Illuminate\Foundation\Application
-   */
-  public function createApplication()
-  {
-    $app = require __DIR__.'/../bootstrap/app.php';
+    /**
+     * The base URL to use while testing the application.
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
 
-    $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
-    return $app;
-  }
+    /**
+     * Creates the application.
+     *
+     * @return mixed
+     */
+    public function createApplication()
+    {
+        $app = include __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+
+    }
+
+
 }
